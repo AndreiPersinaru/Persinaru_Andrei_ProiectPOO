72466fc930d0f9fb1a134c9bba41fe244ff1726a
AndreiPersinaru <147772539+AndreiPersinaru@users.noreply.github.com>
2023-11-01 11:17:10 +0200

    Update Proiect.cpp

--- Proiect.cpp
+++ Proiect.cpp
@@ -1,10 +1,10 @@
-//Domeiu - papetarie
+//Domeniu - papetarie
 #include <iostream>
 
 using namespace std;
 
 class Hartie {
-public:
+private:
 	const int idProdus;
 	static string magazin;
 	int nrMarciInStoc;
@@ -12,6 +12,59 @@ public:
 	float lungime;
 	float latime;
 
+public:
+//Get-eri si Set-eri
+	int getIdProdus() {
+		return idProdus;
+	}
+
+	static void setMagazin(string magazin) {
+		Hartie::magazin = magazin;
+	}
+	string getMagazin() {
+		return magazin;
+	}
+
+	void setNrMarciSiPret(int nrMarciInStoc, float* pretProdus) {
+		if (nrMarciInStoc > 0) {
+			this->nrMarciInStoc = nrMarciInStoc;
+			if (this->pretProdus != NULL) {
+				delete[]this->pretProdus;
+			}
+			this->pretProdus = new float[this->nrMarciInStoc];
+			for (int i = 0; i < this->nrMarciInStoc; i++) {
+				this->pretProdus[i] = pretProdus[i];
+			}
+		}
+	}
+	int getNrMarciInStoc() {
+		return nrMarciInStoc;
+	}
+
+	void setPretProdus(float* pretProdus) {
+		for (int i = 0; i < nrMarciInStoc; i++) {
+			this->pretProdus[i] = pretProdus[i];
+		}
+	}
+	float* getPretProdus() {
+		return pretProdus;
+	}
+
+	void setLungime(float lungime) {
+		this->lungime = lungime;
+	}
+	float getLungime() {
+		return lungime;
+	}
+
+	void setLatime(float latime) {
+		this->latime = latime;
+	}
+	float getLatime() {
+		return latime;
+	}
+
+//Constructori si destructor
 	Hartie() :idProdus(1) {
 		this->nrMarciInStoc = 0;
 		this->pretProdus = NULL;
@@ -33,9 +86,23 @@ public:
 			this->pretProdus[i] = pretProdus[i];
 		}
 	}
+	Hartie(const Hartie& hartie):idProdus(hartie.idProdus) {
+		nrMarciInStoc = hartie.nrMarciInStoc;
+		lungime = hartie.lungime;
+		latime = hartie.latime;
+		if (pretProdus != NULL) {
+			delete[]pretProdus;
+		}
+		pretProdus = new float[hartie.nrMarciInStoc];
+		for (int i = 0; i < hartie.nrMarciInStoc; i++) {
+			pretProdus[i] = hartie.pretProdus[i];
+		}
 
-	static void setMagazin(string magazin) {
-		Hartie::magazin = magazin;
+	}
+	~Hartie() {
+		if (pretProdus != NULL) {
+			delete[] pretProdus;
+		}
 	}
 
 	void afisare() {
@@ -48,11 +115,11 @@ public:
 		}
 		else cout << "-" << endl;
 	}
-
+	friend float pretMediuHartie(const Hartie& hartie);
 };
 
 class Pix {
-public:
+private:
 	const int idProdus;
 	static string magazin;
 	int nrMarciInStoc;
@@ -60,6 +127,59 @@ public:
 	bool pixCuGel;
 	string culoare;
 
+public:
+//Get-eri si Set-eri
+	int getIdProdus() {
+		return idProdus;
+	}
+
+	static void setMagazin(string magazin) {
+		Pix::magazin = magazin;
+	}
+	string getMagazin() {
+		return magazin;
+	}
+
+	void setNrMarciSiPret(int nrMarciInStoc, float* pretProdus) {
+		if (nrMarciInStoc > 0) {
+			this->nrMarciInStoc = nrMarciInStoc;
+			if (this->pretProdus != NULL) {
+				delete[]this->pretProdus;
+			}
+			this->pretProdus = new float[this->nrMarciInStoc];
+			for (int i = 0; i < this->nrMarciInStoc; i++) {
+				this->pretProdus[i] = pretProdus[i];
+			}
+		}
+	}
+	int getNrMarciInStoc() {
+		return nrMarciInStoc;
+	}
+
+	void setPretProdus(float* pretProdus) {
+		for (int i = 0; i < nrMarciInStoc; i++) {
+			this->pretProdus[i] = pretProdus[i];
+		}
+	}
+	float* getPretProdus() {
+		return pretProdus;
+	}
+
+	void setPixCuGel(bool pixCuGel) {
+		this->pixCuGel = pixCuGel;
+	}
+	bool getPixCuGel() {
+		return pixCuGel;
+	}
+
+	void setCuloare(string culoare) {
+		this->culoare = culoare;
+	}
+	string getCuloare() {
+		return culoare;
+	}
+
+//Constructori si destructor
 	Pix() :idProdus(2) {
 		this->nrMarciInStoc = 0;
 		this->pretProdus = NULL;
@@ -81,9 +201,22 @@ public:
 			this->pretProdus[i] = pretProdus[i];
 		}
 	}
-
-	static void setMagazin(string magazin) {
-		Pix::magazin = magazin;
+	Pix(const Pix& pix) :idProdus(pix.idProdus) {
+		nrMarciInStoc = pix.nrMarciInStoc;
+		pixCuGel = pix.pixCuGel;
+		culoare = pix.culoare;
+		if (pretProdus != NULL) {
+			delete[]pretProdus;
+		}
+		pretProdus = new float[pix.nrMarciInStoc];
+		for (int i = 0; i < pix.nrMarciInStoc; i++) {
+			pretProdus[i] = pix.pretProdus[i];
+		}
+	}
+	~Pix() {
+		if (pretProdus != NULL) {
+			delete[] pretProdus;
+		}
 	}
 
 	void afisare() {
@@ -101,18 +234,71 @@ public:
 		}
 		else cout << "-" << endl;
 	}
-
+	friend void generareFactura(const Pix& pix, int cantitateCumparata, int indexProdus);
 };
 
 class Caiet {
-public:
+private:
 	const int idProdus;
 	static string magazin;
 	int nrMarciInStoc;
 	float* pretProdus;
 	int nrFile;
 	string tipCaiet;
+	
+public:
+//Get-eri si Set-eri
+	int getIdProdus() {
+		return idProdus;
+	}
 
+	static void setMagazin(string magazin) {
+		Caiet::magazin = magazin;
+	}
+	string getMagazin() {
+		return magazin;
+	}
+
+	void setNrMarciSiPret(int nrMarciInStoc, float* pretProdus) {
+		if (nrMarciInStoc > 0) {
+			this->nrMarciInStoc = nrMarciInStoc;
+			if (this->pretProdus != NULL) {
+				delete[]this->pretProdus;
+			}
+			this->pretProdus = new float[this->nrMarciInStoc];
+			for (int i = 0; i < this->nrMarciInStoc; i++) {
+				this->pretProdus[i] = pretProdus[i];
+			}
+		}
+	}
+	int getNrMarciInStoc() {
+		return nrMarciInStoc;
+	}
+
+	void setPretProdus(float* pretProdus) {
+		for (int i = 0; i < nrMarciInStoc; i++) {
+			this->pretProdus[i] = pretProdus[i];
+		}
+	}
+	float* getPretProdus() {
+		return pretProdus;
+	}
+
+	void setNrFile(int nrFile) {
+		this->nrFile = nrFile;
+	}
+	int getNrFIle() {
+		return nrFile;
+	}
+
+	void setTipCaiet(string tipCaiet) {
+		this->tipCaiet = tipCaiet;
+	}
+	string getTipCaiet() {
+		return tipCaiet;
+	}
+
+//Constructori si destructor
 	Caiet():idProdus(3){
 		this->nrMarciInStoc = 0;
 		this->pretProdus = NULL;
@@ -136,9 +322,22 @@ public:
 			}
 		}
 	}
-
-	static void setMagazin(string magazin) {
-		Caiet::magazin = magazin;
+	Caiet(const Caiet& caiet):idProdus(caiet.idProdus) {
+		nrMarciInStoc = caiet.nrMarciInStoc;
+		nrFile = caiet.nrFile;
+		tipCaiet = caiet.tipCaiet;
+		if (pretProdus != NULL) {
+			delete[]pretProdus;
+		}
+		pretProdus = new float[caiet.nrMarciInStoc];
+		for (int i = 0; i < caiet.nrMarciInStoc; i++) {
+			pretProdus[i] = caiet.pretProdus[i];
+		}
+	}
+	~Caiet() {
+		if (pretProdus != NULL) {
+			delete[] pretProdus;
+		}
 	}
 
 	void afisare() {
@@ -151,8 +350,26 @@ public:
 		}
 		else cout << "-" << endl;
 	}
-
 };
+
+float pretMediuHartie(const Hartie& hartie) {
+	if (hartie.nrMarciInStoc > 0) {
+		float suma = 0;
+		for (int i = 0; i < hartie.nrMarciInStoc; i++) {
+			suma += hartie.pretProdus[i];
+		}
+		return (suma / hartie.nrMarciInStoc);
+	}
+	else return 0;
+}
+
+void generareFactura(const Pix& pix, int cantitateCumparata, int indexProdus) {
+	if (cantitateCumparata > 0 && indexProdus > 0 && indexProdus < pix.nrMarciInStoc) {
+		float total = pix.pretProdus[indexProdus] * cantitateCumparata;
+		string pixuri = (cantitateCumparata == 1) ? " pix " : " pixuri ";
+		cout << "Factura:" << endl << "Ati achizitionat " << cantitateCumparata << pixuri << "la pret de " << pix.pretProdus[indexProdus] << "RON. " << "Totalul este de: " << total << "RON." << endl;
+	}
+}
 
 string Hartie::magazin = "Papetarie S.R.L.";
 string Pix::magazin = "Papetarie S.R.L.";
@@ -160,49 +377,102 @@ string Caiet::magazin = "Papetarie S.R.L.";
 
 void main() {
 //HARTIE - Initializarea obiectelor
-	float* pretHartie=new float[3];
-	pretHartie[0] = 13.99;
-	pretHartie[1] = 14.99;
-	pretHartie[2] = 19.99;
+	float* pretHartie = new float[3] {13.99, 14.99, 19.99};
 	Hartie hartie1;
 	Hartie hartie2(100);
 	Hartie hartie3(100, 3, pretHartie, 29.7, 21);
+	Hartie hartie4(hartie2);
 //Afisare
 	hartie1.afisare();
 	hartie2.afisare();
 	hartie3.afisare();
 	hartie3.setMagazin("magazinul Papetarie S.R.L.");
 	hartie3.afisare();
+	hartie4.afisare();
+//Set-eri
+	hartie4.setNrMarciSiPret(3, pretHartie);
+	hartie4.setPretProdus(pretHartie);
+	hartie4.setLungime(29.7);
+	hartie4.setLatime(21);
+//Get-eri
+	cout << "Get-er ID: " << hartie4.getIdProdus() << endl;
+	cout << "Get-er magazin: " << hartie4.getMagazin() << endl;
+	cout << "Get-er numar marci in stoc: " << hartie4.getNrMarciInStoc() << endl;
+	cout << "Get-er preturi produs: ";
+	for (int i = 0; i < hartie4.getNrMarciInStoc(); i++) {
+		cout << hartie4.getPretProdus()[i] << " ";
+	}
 	cout << endl;
+	cout << "Get-er lungime: " << hartie4.getLungime() << endl;
+	cout << "Get-er latime: " << hartie4.getLatime() << endl;
+//Functie globala friend
+	cout << "Pretul mediu pentru hartie este de: " << pretMediuHartie(hartie4) << "RON" << endl << endl;
+
 
 //PIX - Initializarea obiectelor
-	float* pretPix = new float[4];
-	pretPix[0] = 2.99;
-	pretPix[1] = 3.50;
-	pretPix[2] = 4.99;
-	pretPix[3] = 8.00;
+	float* pretPix = new float[4] {2.99, 3.50, 4.99, 8.00};
 	Pix pix1;
 	Pix pix2(200);
 	Pix pix3(200, 4, pretPix, true, "negru");
+	Pix pix4(pix2);
 //Afisare
 	pix1.afisare();
 	pix2.afisare();
 	pix3.afisare();
 	pix3.setMagazin("magazinul Papetarie S.R.L.");
 	pix3.afisare();
+	pix4.afisare();
+//Set-eri
+	pix4.setNrMarciSiPret(4, pretPix);
+	pix4.setPretProdus(pretPix);
+	pix4.setPixCuGel(true);
+	pix4.setCuloare("negru");
+//Get-eri
+	cout << "Get-er ID: " << pix4.getIdProdus() << endl;
+	cout << "Get-er magazin: " << pix4.getMagazin() << endl;
+	cout << "Get-er numar marci in stoc: " << pix4.getNrMarciInStoc() << endl;
+	cout << "Get-er preturi produs: ";
+	for (int i = 0; i < pix4.getNrMarciInStoc(); i++) {
+		cout << pix4.getPretProdus()[i] << " ";
+	}
+	cout << endl;
+	cout << "Get-er pix cu gel: ";
+	if (pix4.getPixCuGel()) {
+		cout << "Pix cu gel" << endl;
+	}
+	else cout << "Pix fara gel" << endl;
+	cout << "Get-er culoare: " << pix4.getCuloare() << endl;
+//Functie globala friend
+	generareFactura(pix4, 5, 2);
 	cout << endl;
 
 //CAIET - Initializarea obiectelor
-	float* pretCaiet = new float[2];
-	pretCaiet[0] = 9.99;
-	pretCaiet[1] = 17.50;
+	float* pretCaiet = new float[2] {9.99, 17.50};
 	Caiet caiet1;
 	Caiet caiet2(300);
 	Caiet caiet3(300, 2, pretCaiet, 80, "velin");
+	Caiet caiet4(caiet2);
 //Afisare
 	caiet1.afisare();
 	caiet2.afisare();
 	caiet3.afisare();
 	caiet3.setMagazin("magazinul Papetarie S.R.L.");
 	caiet3.afisare();
+	caiet4.afisare();
+//Set-eri
+	caiet4.setNrMarciSiPret(2, pretCaiet);
+	caiet4.setPretProdus(pretCaiet);
+	caiet4.setNrFile(80);
+	caiet4.setTipCaiet("velin");
+//Get-eri
+	cout << "Get-er ID: " << caiet4.getIdProdus() << endl;
+	cout << "Get-er magazin: " << caiet4.getMagazin() << endl;
+	cout << "Get-er numar marci in stoc: " << caiet4.getNrMarciInStoc() << endl;
+	cout << "Get-er preturi produs: ";
+	for (int i = 0; i < caiet4.getNrMarciInStoc(); i++) {
+		cout << caiet4.getPretProdus()[i] << " ";
+	}
+	cout << endl;
+	cout << "Get-er numar file: " << caiet4.getNrFIle() << endl;
+	cout << "Get-er tip caiet: " << caiet4.getTipCaiet() << endl << endl;
 }
